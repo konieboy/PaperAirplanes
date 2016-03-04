e4629a0b94de28b4124ee22272e412f1ff19ae65
konieboy <konieboy@gmail.com>
2016-03-04 13:50:57 -0700

    Added files via upload
    woops

--- src/Client.java
+++ src/Client.java
@@ -3,13 +3,40 @@
 //Brendan Petras, Ethan Hamman, Konrad Wisniewski, Kyle Sutherland
 
 //Imports
-
+import java.io.*;
 
 /**
  *  Client class facilitates communication with the server and
  *  allows the user to send commands to the server for starting
  *  chats
  */
-public class Client{
+public class Client
+{
+	public static void main(String[] args)
+	{
+		BufferedReader  userInput = new BufferedReader(new InputStreamReader(System.in));
+		String line;
+		System.out.println("Welcome to paper airplanes!");
+	   while (true)
+		{
+			try
+			{
+		   	System.out.print("Paper Planes: ");
+				line = userInput.readLine();
+				System.out.println(line);
 
+				//check if first letter of string is a "/"
+				if (line.substring(0, 1) == "/")
+				{
+					System.out.println("Special Command!");
+				}
+
+			}
+			catch (IOException ioe)
+			{
+				System.out.println("Invalid Command!");
+			}
+
+		}
+	}
 }
