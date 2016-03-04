5f6b2cdc841a7a7caf5dbf369fc6bb6938061d85
Kyle Sutherland <kysuther@live.ca>
2016-03-04 13:37:21 -0700

    added base for user accounts

--- src/User.java
+++ src/User.java
@@ -4,6 +4,7 @@
 
 //Imports
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  *  User class keeps track of the users profile while they are logged into
@@ -17,16 +18,51 @@ import java.util.ArrayList;
 public class User{
   private int userID;
   private String username;
+  private String password;
   private String passwordHash;
   private String rsaPubKey;
   private String rsaPrivateKey;
   private ArrayList<String> friendsList;
 
   public User(){
+      userID = -1;
+      username = "";
+      password = "";
+      passwordHash = "";
+      rsaPubKey = "";
+      rsaPrivateKey = "";
+      friendsList = null;
 
+      initNewUser();
   }
 
-  public User createNewUser(int userID){
-      return null;
+  public void initNewUser(){
+    //If user profile is found in directory:
+    //loadSavedUser(fileToProfile)
+
+    //Else:
+    //Ask if they want to login
+      //getUserFromServer(username)
+    //or register
+      //createNewUser();
+  }
+
+  public void createNewUser(){
+      Scanner userIn = new Scanner(System.in);
+
+      //Pick username
+      System.out.print("Choose a username: ");
+      username = userIn.nextLine();
+
+      //Pick password
+      System.out.print("Choose a password: ");
+      password = userIn.nextLine();
+      //Hash password - Implementing later
+
+      //Generate RSA keys - Implementing later
+  }
+
+  public void loadSavedUser(String inputFile){
+
   }
 }
