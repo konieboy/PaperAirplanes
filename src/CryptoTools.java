614d05f87615f22c7ce667c167511114b6d93b9a
Kyle Sutherland <kysuther@live.ca>
2016-03-29 17:53:17 -0600

    added the crypto file, to be implemented later

---
+++ src/CryptoTools.java
@@ -0,0 +1,23 @@
+//CPSC 441 W2016 Group 31
+//Paper Airplanes Messenger
+//Brendan Petras, Ethan Hamman, Konrad Wisniewski, Kyle Sutherland
+
+import javax.crypto.*;
+import java.security.*;
+
+public class CryptoTools{
+    private String keySeed;
+    private Cipher aesCipher;
+
+    public CryptoTools(){
+        try{
+            aesCipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        }catch(Exception e){
+            System.out.println("Algorithm does not exist.");
+        }
+    }
+
+    public byte[] encryptMessage(String messageIn, String keySeed){
+        return messageIn.getBytes();
+    }
+}
