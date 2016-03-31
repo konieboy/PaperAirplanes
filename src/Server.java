92d53cef3f8998c3800db1b0fd32db63df96caff
echamman <echamman@ucalgary.ca>
2016-03-31 11:44:21 -0600

    User writes when disconnect, tabs good

--- src/Server.java
+++ src/Server.java
@@ -18,236 +18,223 @@ import java.util.*;
  */
 public class Server
 {
-  private static ArrayList<User> usersOnline = new ArrayList<User>();
-  private static ArrayList<Integer> connectedClientID = new ArrayList<Integer>();
-  private static ArrayList<RoomServer> currentRooms = new ArrayList<RoomServer>();
-  private static int BUFFERSIZE = 256;
-  private static final String filePath = "users/";
+    private static ArrayList<User> usersOnline = new ArrayList<User>();
+    private static ArrayList<Integer> connectedClientID = new ArrayList<Integer>();
+    private static ArrayList<RoomServer> currentRooms = new ArrayList<RoomServer>();
+    private static int BUFFERSIZE = 256;
+    private static final String filePath = "users/";
 
-  //Return false: not in list of registeredUsers
-  //Return true: in list of registeredUsers
-
-
-
-  public static void main(String args[])
-  {
-
-	  if(args.length != 1)
-	  {
-		  System.out.println("Invalid Input");
-		  System.out.println("Requires <Port Number>");
-		  System.exit(0);
-	  }
-
-    // Initialize buffers and coders for channel receive and send
-    int portNumber = Integer.parseInt(args[0]);
-    String line = "";
-    Charset charset = Charset.forName( "us-ascii" );
-    CharsetDecoder decoder = charset.newDecoder();
-    CharsetEncoder encoder = charset.newEncoder();
-    ByteBuffer inBuffer = null;
-    CharBuffer cBuffer = null;
-    int bytesSent, bytesRecv;     // number of bytes sent or received
-    FileInputStream infile = null;
-
-    DataOutputStream clientWriter;
+    //Return false: not in list of registeredUsers
+    //Return true: in list of registeredUsers
 
 
 
+    public static void main(String args[])
+    {
 
-    try{
-       //trying to create new socket, default port for Project is 3265.
-       Selector selector = Selector.open();
-
-      //Open new socketchannel and make it non-blocking
-       ServerSocketChannel channel = ServerSocketChannel.open();
-       channel.configureBlocking(false);
-
-       //Bind to port
-       InetSocketAddress isa = new InetSocketAddress(portNumber);
-       channel.socket().bind(isa);
-
-       //Register select server, wait for connection requests
-       channel.register(selector, SelectionKey.OP_ACCEPT);
-
-        // Wait for something happen among all registered sockets
-        boolean terminated = false;
-        while (!terminated)
+        if(args.length != 1)
         {
-        	if (selector.select(500) < 0)
+            System.out.println("Invalid Input");
+            System.out.println("Requires <Port Number>");
+            System.exit(0);
+        }
+
+        // Initialize buffers and coders for channel receive and send
+        int portNumber = Integer.parseInt(args[0]);
+        String line = "";
+        Charset charset = Charset.forName( "us-ascii" );
+        CharsetDecoder decoder = charset.newDecoder();
+        CharsetEncoder encoder = charset.newEncoder();
+        ByteBuffer inBuffer = null;
+        CharBuffer cBuffer = null;
+        int bytesSent, bytesRecv;     // number of bytes sent or received
+        FileInputStream infile = null;
+
+        DataOutputStream clientWriter;
+
+
+
+
+        try
+        {
+           //trying to create new socket, default port for Project is 3265.
+           Selector selector = Selector.open();
+
+          //Open new socketchannel and make it non-blocking
+           ServerSocketChannel channel = ServerSocketChannel.open();
+           channel.configureBlocking(false);
+
+           //Bind to port
+           InetSocketAddress isa = new InetSocketAddress(portNumber);
+           channel.socket().bind(isa);
+
+           //Register select server, wait for connection requests
+           channel.register(selector, SelectionKey.OP_ACCEPT);
+
+            // Wait for something happen among all registered sockets
+            boolean terminated = false;
+            while (!terminated)
             {
-                System.out.println("select() failed");
-                System.exit(1);
-            }
-        Thread.sleep(100);
-            // Get set of ready sockets
-            Set readyKeys = selector.selectedKeys();
-            Iterator readyItor = readyKeys.iterator();
-
-            // Walk through the ready set
-            while (readyItor.hasNext())
-            {
-                // Get key from set
-                SelectionKey key = (SelectionKey)readyItor.next();
-
-                // Remove current entry
-                readyItor.remove();
-
-                // Accept new connections, if any
-                if (key.isAcceptable())
+            	if (selector.select(500) < 0)
                 {
-
-                    SocketChannel cchannel = ((ServerSocketChannel)key.channel()).accept();
-                    cchannel.configureBlocking(false);
-                    System.out.println("Accept connection from " + cchannel.socket().toString());
-
-                    // Register the new connection for read operation
-                    cchannel.register(selector, SelectionKey.OP_READ);
-
+                    System.out.println("select() failed");
+                    System.exit(1);
                 }
-               else{
-                    SocketChannel cchannel = (SocketChannel)key.channel();
-                    if (key.isReadable()){
-                        Socket socket = cchannel.socket();
+            Thread.sleep(100);
+                // Get set of ready sockets
+                Set readyKeys = selector.selectedKeys();
+                Iterator readyItor = readyKeys.iterator();
 
-                        // Open input and output streams
-                        inBuffer = ByteBuffer.allocateDirect(BUFFERSIZE);
-                        cBuffer = CharBuffer.allocate(BUFFERSIZE);
+                // Walk through the ready set
+                while (readyItor.hasNext())
+                {
+                    // Get key from set
+                    SelectionKey key = (SelectionKey)readyItor.next();
 
-                        // Read from socket
-                        bytesRecv = cchannel.read(inBuffer);
-                        if (bytesRecv <= 0){
-                            System.out.println("read() error, or connection closed");
-                            key.cancel();  //deregister the socket
-                            continue;
-                        }
+                    // Remove current entry
+                    readyItor.remove();
 
-                        inBuffer.flip();      // make buffer available
-                        decoder.decode(inBuffer, cBuffer, false);
-                        cBuffer.flip();
-                        line = cBuffer.toString();
-                        System.out.println("TCP Client: " + line);
+                    // Accept new connections, if any
+                    if (key.isAcceptable())
+                    {
 
-                        //////**********************************
-                        //respond to client input
+                        SocketChannel cchannel = ((ServerSocketChannel)key.channel()).accept();
+                        cchannel.configureBlocking(false);
+                        System.out.println("Accept connection from " + cchannel.socket().toString());
 
-                        //detect that the client wants to add a new user
-                        if(line.contains("/add "))
-                        {
+                        // Register the new connection for read operation
+                        cchannel.register(selector, SelectionKey.OP_READ);
 
+                    }
+                   else{
+                        SocketChannel cchannel = (SocketChannel)key.channel();
+                        if (key.isReadable()){
+                            Socket socket = cchannel.socket();
 
-                        	line = line.replace("/add ","");
-                            System.out.println("Checking to make sure that " + line + " is a registered user...");
+                            // Open input and output streams
+                            inBuffer = ByteBuffer.allocateDirect(BUFFERSIZE);
+                            cBuffer = CharBuffer.allocate(BUFFERSIZE);
 
-                            //add user to the friend list
-                            addFriend(line, getUser(cchannel.socket().getPort()));
-                            //printFriendList(cchannel, encoder);
-
-                        }
-                        else if (line.contains("/printfriends" ))
-                        {
-                            line = line.replace("/printfriends ", "");
-                            String friends = printFriendList(getUser(cchannel.socket().getPort()));
-                            sendMessage(friends, cchannel, encoder);
-                        }
-                        else if(line.contains("/remove "))
-                        {
-                            line = line.replace("/remove ", "");
-                            //removeFriend(line,cchannel, encoder);
-                            //printFriendList(cchannel, encoder);
-
-                        }
-
-                        else if(line.contains("/userdata"))
-                        {
-                            String[] up = line.split(" ");
-                            if(checkUser(up[1], up[2])){
-                                System.out.println(cchannel.socket().getPort());
-                                usersOnline.add(new User(filePath + up[1]+".txt", cchannel.socket().getPort()));
+                            // Read from socket
+                            bytesRecv = cchannel.read(inBuffer);
+                            if (bytesRecv <= 0){
+                                System.out.println("read() error, or connection closed");
+                                User outUser = getUser(cchannel.socket().getPort());    //Write to user
+                                String toFile = outUser.toString();
+                                try
+                                {
+                                    Writer wr = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(filePath+outUser.getUserName()+".txt"), "UTF-8"));
+                                    wr.write(toFile);
+                                    wr.close();
+                                }
+                                catch(Exception f)
+                                {
+                                    f.printStackTrace();
+                                }
+                                key.cancel();  //deregister the socket
+                                continue;
                             }
-                        }
-                        else if(line.contains("/quit")){
-                            User outUser = getUser(cchannel.socket().getPort());
-                            String toFile = outUser.toString();
-                            try
+
+                            inBuffer.flip();      // make buffer available
+                            decoder.decode(inBuffer, cBuffer, false);
+                            cBuffer.flip();
+                            line = cBuffer.toString();
+                            System.out.println("TCP Client: " + line);
+
+                            //////**********************************
+                            //respond to client input
+
+                            //detect that the client wants to add a new user
+                            if(line.contains("/add "))
                             {
-                                Writer wr = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(filePath+outUser.getUserName()+".txt"), "UTF-8"));
-                                wr.write(toFile);
-                                wr.close();
+
+
+                            	line = line.replace("/add ","");
+                                System.out.println("Checking to make sure that " + line + " is a registered user...");
+
+                                //add user to the friend list
+                                addFriend(line, getUser(cchannel.socket().getPort()));
+                                //printFriendList(cchannel, encoder);
+
                             }
-                            catch(Exception f)
+                            else if (line.contains("/printfriends" ))
                             {
-                                f.printStackTrace();
+                                line = line.replace("/printfriends ", "");
+                                String friends = printFriendList(getUser(cchannel.socket().getPort()));
+                                sendMessage(friends, cchannel, encoder);
+                            }
+                            else if(line.contains("/remove "))
+                            {
+                                line = line.replace("/remove ", "");
+                                //removeFriend(line,cchannel, encoder);
+                                //printFriendList(cchannel, encoder);
+
+                            }
+
+                            else if(line.contains("/userdata"))
+                            {
+                                String[] up = line.split(" ");
+                                if(checkUser(up[1], up[2])){
+                                    System.out.println(cchannel.socket().getPort());
+                                    usersOnline.add(new User(filePath + up[1]+".txt", cchannel.socket().getPort()));
+                                }
+                            }
+                            else if(line.contains("/quit")){
+                                User outUser = getUser(cchannel.socket().getPort());
+                                String toFile = outUser.toString();
+                                try
+                                {
+                                    Writer wr = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(filePath+outUser.getUserName()+".txt"), "UTF-8"));
+                                    wr.write(toFile);
+                                    wr.close();
+                                }
+                                catch(Exception f)
+                                {
+                                    f.printStackTrace();
+                                }
+                            }
+                            else
+                            {
+                               //Not a command
+                               //cchannel.write(encoder.encode(CharBuffer.wrap("Command not recognized: " + line + "end\n")));
                             }
                         }
-                        else
-                        {
-                           //Not a command
-                           //cchannel.write(encoder.encode(CharBuffer.wrap("Command not recognized: " + line + "end\n")));
-                        }
+                    }
+                } // end of while (readyItor.hasNext())
+            } // end of while (!terminated)
 
-
-                     }
-                }
-            } // end of while (readyItor.hasNext())
-        } // end of while (!terminated)
-
-        // close all connections
-        Set keys = selector.keys();
-        Iterator itr = keys.iterator();
-        while (itr.hasNext())
+            // close all connections
+            Set keys = selector.keys();
+            Iterator itr = keys.iterator();
+            while (itr.hasNext())
+            {
+                SelectionKey key = (SelectionKey)itr.next();
+                //itr.remove();
+                if (key.isAcceptable())
+                    ((ServerSocketChannel)key.channel()).socket().close();
+                else if (key.isValid())
+                    ((SocketChannel)key.channel()).socket().close();
+            }
+        }
+        catch (Exception e)
         {
-            SelectionKey key = (SelectionKey)itr.next();
-            //itr.remove();
-            if (key.isAcceptable())
-                ((ServerSocketChannel)key.channel()).socket().close();
-            else if (key.isValid())
-                ((SocketChannel)key.channel()).socket().close();
+            e.printStackTrace();
         }
     }
 
-    catch (Exception e)
+    public RoomServer startRoomServer()
     {
-        e.printStackTrace();
+        return null;
     }
-  }
 
-  public RoomServer startRoomServer()
-  {
-      return null;
-  }
+    public void sendMessage(String msg)
+    {
+        return;
+    }
 
-  public void sendMessage(String msg)
-  {
-     return;
-  }
-
-  public byte[] receiveMessage()
-  {
-      return null;
-  }
-
-  //Delete??????
-  private static Boolean searchForName(String name) {
-
-	  try
-	  {
-		  File file = new File("registeredUsers.txt");
-		  Scanner input = new Scanner(file);
-		  while(input.hasNextLine()) {
-			  String lineFromFile = input.nextLine();
-			  if (lineFromFile.equals(name))
-			  {
-				  return true;
-			  }
-		   }
-
-	  }
-	  catch (IOException e)
-	    {
-	        e.printStackTrace();
-	    }
-	  return false;
+    public byte[] receiveMessage()
+    {
+        return null;
     }
 
     //Check if a user exists at all
@@ -273,35 +260,35 @@ public class Server
         return null;
     }
 
-  private static boolean checkUser(String username, String passwordHash){
-      try{
-          BufferedReader br = new BufferedReader(new FileReader(filePath+username+".txt"));
-          br.readLine();
-          if(passwordHash.equals(br.readLine())){
-              System.out.println("Login Successful");
-              return true;
-          }
-          else{
-              System.out.println("Scum user couldn't log in");
-              return false;
-          }
-      }catch(FileNotFoundException fe){
-          try{
-              Writer wr = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(filePath+username+".txt"), "UTF-8"));
-              wr.write(username+"\n");
-              wr.write(passwordHash+"\n");
-              System.out.println("Profile Created");
-              wr.close();
-              return true;
-          }catch(IOException ie){
-              return false;
-          }
-      }catch(IOException ioe){
-          return false;
-      }
-  }
+    private static boolean checkUser(String username, String passwordHash){
+        try{
+            BufferedReader br = new BufferedReader(new FileReader(filePath+username+".txt"));
+            br.readLine();
+            if(passwordHash.equals(br.readLine())){
+                System.out.println("Login Successful");
+                return true;
+            }
+            else{
+                System.out.println("Scum user couldn't log in");
+                return false;
+            }
+        }catch(FileNotFoundException fe){
+            try{
+                Writer wr = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(filePath+username+".txt"), "UTF-8"));
+                wr.write(username+"\n");
+                wr.write(passwordHash+"\n");
+                System.out.println("Profile Created");
+                wr.close();
+                return true;
+            }catch(IOException ie){
+                return false;
+            }
+        }catch(IOException ioe){
+            return false;
+        }
+    }
 
-  // sendMessage - sends string to client through TCP socketchannel
+    // sendMessage - sends string to client through TCP socketchannel
     private static int sendMessage(String msg, SocketChannel cchannel, CharsetEncoder encoder) throws IOException
     {
         int outLen = msg.length();
