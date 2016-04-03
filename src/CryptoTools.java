6c4277b2d10b4458ed110f71764dd960460eb040
Kyle Sutherland <kysuther@live.ca>
2016-04-02 18:06:41 -0600

    cryptotools documentation

--- src/CryptoTools.java
+++ src/CryptoTools.java
@@ -72,11 +72,20 @@ public class CryptoTools{
         return output;
     }
 
+    /**
+     *  decryptString takes in a string and a key and decrypts the string.
+     *  the string it takes in should be a string representation of a
+     *  string encrypted with the same key.
+     */
     public String decryptString(String strIn, String keySeedIn) throws Exception{
         byte[] strArray = arrayStrToArray(strIn);
         return new String(decryptMessage(strArray, keySeedIn));
     }
 
+    /**
+     *  Decrypt message takes in a byte array and a key and decrypts them
+     *  using the AES-CBC algorithm.  It returns the message as a byte array.
+     */
     public byte[] decryptMessage(byte[] messageIn, String keySeed) throws Exception{
         secKeySpec = getKey(keySeed);
         aesCipher.init(Cipher.DECRYPT_MODE, secKeySpec, ivSpec);
@@ -84,6 +93,10 @@ public class CryptoTools{
         return plaintext;
     }
 
+    /**
+     *  getKey takes in a string for the key and will return a key for the
+     *  AES encryption using the string as a seed in the PRNG.
+     */
     public SecretKeySpec getKey(String seedIn) throws Exception{
         randNum = randNum.getInstance("SHA1PRNG");
         seedBytes = seedIn.getBytes();
@@ -93,6 +106,11 @@ public class CryptoTools{
         return secKeySpec;
     }
 
+    /**
+     *  hashPassword takes in a plaintext password and uses a default java
+     *  password hashing algorithm to generate a cryptographically secure
+     *  hash that will be stored on the users profile on the server.
+     */
     public String hashPassword(String password) throws Exception{
         byte[] salt = new byte[16];
         randNum.nextBytes(salt);
@@ -100,6 +118,10 @@ public class CryptoTools{
         return output;
     }
 
+    /**
+     *  hashPasswordHelper takes in extra parameters in order to use the
+     *  algorith to generate the cryptographic hash.
+     */
     public String hashPasswordHelper(String password, byte[] salt, int iterations, int hashLength) throws Exception{
         PBEKeySpec keySpec = new PBEKeySpec(password.toCharArray(), salt, iterations, hashLength);
         SecretKeyFactory keyFac = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
@@ -107,6 +129,7 @@ public class CryptoTools{
 
         byte[] outputArr = new byte[salt.length + hash.length];
 
+        //combine the salt and the hash into one output array
         int arrCounter = 0;
         for(byte b : salt){
             outputArr[arrCounter] = b;
@@ -116,15 +139,24 @@ public class CryptoTools{
             arrCounter++;
         }
 
+        //convert the output array to a base64 string and return it
         String outputString = bytesToBase64(outputArr);
         return outputString;
     }
 
+    /**
+     *  verifyPassword takes in a plaintext password and a salted hash
+     *  and returns whether the password matches the hash based on
+     *  what the salt and hash are.
+     *
+     *  returns true if password matches, false otherwise
+     */
     public Boolean verifyPassword(String password, String saltyHash) throws Exception{
         byte[] salt = new byte[16];
         byte[] saltHashArr = base64toBytes(saltyHash);
         byte[] hashIn = new byte[saltHashArr.length - salt.length];
 
+        //split the salt and the hash from the salted hash parameter.
         int arrCounter = 0;
         for(int i = 0; i < salt.length; i++){
             salt[i] = saltHashArr[arrCounter];
@@ -135,22 +167,34 @@ public class CryptoTools{
         }
         System.out.println(hashIn.length);
 
+        //initialize the algorithm tools.
         PBEKeySpec keySpec = new PBEKeySpec(password.toCharArray(), salt, iterations, 128);
         SecretKeyFactory keyFac = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
         byte[] calcedHash = keyFac.generateSecret(keySpec).getEncoded();
 
+        //make input hash and calced hash into base64 strings
         String hashInStr = bytesToBase64(hashIn);
         String calcedHashStr = bytesToBase64(calcedHash);
 
+        //compare them to see if they match
         if(hashInStr.equals(calcedHashStr))
             return true;
         return false;
     }
 
+    /**
+     *  base64toBytes takes in a string and returns the binary byte
+     *  array.
+     */
     public byte[] base64toBytes(String lineIn) throws Exception{
         return DatatypeConverter.parseBase64Binary(lineIn);
     }
 
+
+    /**
+     *  bytesToBase64 takes in a binary byte array and returns a
+     *  base64 string so that it can be stored cleanly.
+     */
     public String bytesToBase64(byte[] bytesIn) throws Exception{
         return DatatypeConverter.printBase64Binary(bytesIn);
     }
