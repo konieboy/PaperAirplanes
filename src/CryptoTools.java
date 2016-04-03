da050f81c9750c002c000f125b8bcf58d89761b5
Kyle Sutherland <kysuther@live.ca>
2016-04-03 15:41:32 -0600

    back to base64 encryption format

--- src/CryptoTools.java
+++ src/CryptoTools.java
@@ -42,7 +42,8 @@ public class CryptoTools{
      *  encrypted string in the form of a byte array in string format.
      */
     public String encryptString(String strIn, String keySeedIn) throws Exception{
-        return Arrays.toString(encryptMessage(strIn.getBytes(), keySeedIn));
+        //return Arrays.toString(encryptMessage(strIn.getBytes(), keySeedIn));
+        return bytesToBase64(encryptMessage(strIn.getBytes(), keySeedIn));
     }
 
     /**
@@ -80,7 +81,8 @@ public class CryptoTools{
      *  string encrypted with the same key.
      */
     public String decryptString(String strIn, String keySeedIn) throws Exception{
-        byte[] strArray = arrayStrToArray(strIn);
+        //byte[] strArray = arrayStrToArray(strIn);
+        byte[] strArray = base64toBytes(strIn);
         return new String(decryptMessage(strArray, keySeedIn));
     }
 
